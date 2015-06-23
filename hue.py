1de0f4a323a0bd6fd60d5233d6948963e14d6b3a
root <root@raspberrypi.(none)>
2015-06-23 18:09:04 -0400

    up

--- hue.py
+++ hue.py
@@ -1,41 +1,65 @@
-from twilio.rest import TwilioRestClient
+import requests
+import json
 
-# put your own credentials here 
-ACCOUNT_SID = "ACdfe9efe2ba27cf4cec7539813935c656" 
-AUTH_TOKEN = "754f1063d616f4691ca1248abb4644b7" 
+TAG_1 = "3720938632"
+TAG_2 = "3709076616"
+TAG_3 = "3702850696"
 
-CLIENT_TAG = "20801160"
-HOUSEKEEPING_TAG = "3687842952"
+r = requests.get("http://192.168.1.106/api/newdeveloper")
 
-HOTEL_NUMBER = "9172078398"
-CLIENT_NUMBER = "9172078398"
+light_vals = []
 
- 
-#get rfid from hid
+light_vals.append([])
+light_vals.append([])
+light_vals.append([])
+
+light_vals[0].append({"sat": 208, "bri": 207, "hue": 13234})
+light_vals[0].append({"sat": 253, "bri": 230, "hue": 65527})
+light_vals[0].append({"sat": 208, "bri": 207, "hue": 13234})
+
+light_vals[1].append({"sat": 232, "bri": 203, "hue": 34495})
+light_vals[1].append({"sat": 232, "bri": 203, "hue": 34495})
+light_vals[1].append({"sat": 232, "bri": 203, "hue": 34495})
+
+light_vals[2].append({"sat": 0, "bri": 0, "hue": 0})
+light_vals[2].append({"sat": 0, "bri": 0, "hue": 0})
+light_vals[2].append({"sat": 0, "bri": 0, "hue": 0})
+
+#print(r.status_code)
+#print(r.content)
+#_url = "http://192.168.1.106/api/newdeveloper/lights/2/state"
+
+setIndex = 0
+
+def setLights(_setIndex):
+
+  for i in range(0, 3):
+
+    _url = "http://192.168.1.106/api/newdeveloper/lights/" + str(i+1) + "/state"
+    _payload = json.dumps(light_vals[_setIndex][i])
+
+    print(_url)
+    print(_payload)
+
+    r = requests.put(_url, _payload)
+
+    print(r.status_code)
+    print(r.content)
+
 
 while (True):
   _rfid = input("rfid:");
   print(_rfid);
 
-  if str(_rfid) == CLIENT_TAG:
+  if str(_rfid) == TAG_1:
 
-    print("RFID: please clean");
-    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+    setLights(0);
 
-    client.messages.create(
-      to=HOTEL_NUMBER, 
-      from_="+12015499399", 
-      body="Please clean room",  
-    )
+  if str(_rfid) == TAG_2:
 
-  if str(_rfid) == HOUSEKEEPING_TAG:
+    setLights(1);
 
-    print("RFID: done cleaning");
+  if str(_rfid) == TAG_3:
 
-    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+    setLights(2);
 
-    client.messages.create(
-      to=CLIENT_NUMBER, 
-      from_="+12015499399", 
-      body="We have cleaned room",  
-    )
